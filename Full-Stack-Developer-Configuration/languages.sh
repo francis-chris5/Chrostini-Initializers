8b632953b3d5dcf14089a54d38f2e62689a8126b
francis-chris5 <50467171+francis-chris5@users.noreply.github.com>
2021-05-06 19:22:15 -0400

    Add files via upload

--- Full-Stack-Developer-Configuration/languages.sh
+++ Full-Stack-Developer-Configuration/languages.sh
@@ -12,7 +12,7 @@ sudo apt install idle3 -y
                 # check https://www.apachefriends.org/download.html for latest version
 wget https://www.apachefriends.org/xampp-files/7.4.18/xampp-linux-x64-7.4.18-0-installer.run
 sudo chmod 755 xampp-linux-*-installer.run
-sudo ./xampp-linux-*-installer.run -y
+sudo ./xampp-linux-*-installer.run
 rm xampp-linux-*-installer.run
 sudo chmod 777 /opt/lampp/htdocs
 ln -s /opt/lampp/htdocs/
