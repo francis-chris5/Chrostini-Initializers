5bde7d85b6358de0f406aa1ccc9c65cf976d96ac
francis-chris5 <50467171+francis-chris5@users.noreply.github.com>
2021-05-06 19:14:59 -0400

    Add files via upload

---
+++ Full-Stack-Developer-Configuration/languages.sh
@@ -0,0 +1,18 @@
+
+    ### INSTALL LANGUAGES
+        
+        # Java
+sudo apt install libnss3 default-jdk -y
+
+        # Python and IDLE
+sudo apt install idle3 -y
+
+
+        # XAMPP (Apache, PHP, MySQL)
+                # check https://www.apachefriends.org/download.html for latest version
+wget https://www.apachefriends.org/xampp-files/7.4.18/xampp-linux-x64-7.4.18-0-installer.run
+sudo chmod 755 xampp-linux-*-installer.run
+sudo ./xampp-linux-*-installer.run -y
+rm xampp-linux-*-installer.run
+sudo chmod 777 /opt/lampp/htdocs
+ln -s /opt/lampp/htdocs/
---
+++ Full-Stack-Developer-Configuration/workingDirectory.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+
+    ### SET UP WORKING DIRECTORIES
+mkdir ~/Documents
+mkdir ~/Documents/delete
+mkdir ~/Documents/Programs
+ln -s /mnt/chromeos/MyFiles/Downloads/
+
+
