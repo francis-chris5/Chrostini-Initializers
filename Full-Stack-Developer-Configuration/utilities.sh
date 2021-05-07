a52fc43a4a060e786e8f6b6ba4a46daaec32f1eb
francis-chris5 <50467171+francis-chris5@users.noreply.github.com>
2021-05-06 20:19:53 -0400

    Add files via upload

---
+++ Full-Stack-Developer-Configuration/utilities.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+
+    ### INSTALL USEFUL TOOLS
+
+
+        # sublime text editor
+wget https://download.sublimetext.com/sublime_text_3_build_3211_x64.tar.bz2
+tar -xf sublime_text_3_*_x64.tar.bz2 -C ~/Documents/Programs/
+rm sublime_text_3_*_x64.tar.bz2
+mv ~/Documents/Programs/sublime_text_3/sublime_text ~/Documents/Programs/sublime_text_3/sublime
+
+
+
+        # thing to create desktop shortcuts
+sudo apt install menulibre -y
+                #################################
+                ##                             ##
+                ## replace this with actually  ##
+                ## writing the [Desktop Entry] ##
+                ## eventually                  ##
+                ##                             ##
+                #################################
+
+        
+        # browser to test with xampp localhost access
+sudo apt install firefox-esr -y
