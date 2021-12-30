944a21728fbc4d019dd95639f45e1731bd9b57f7
francis-chris5 <50467171+francis-chris5@users.noreply.github.com>
2021-12-30 01:01:46 -0500

    Update data-analysis-python.sh

--- data-analysis-python.sh
+++ data-analysis-python.sh
@@ -18,6 +18,8 @@ sudo apt install python3-pip -y
 sudo pip3 install numpy scipy matplotlib pandas jupyter
 sudo apt install spyder -y
 
+#### WHOOPS! I forgot tqdm and probably gonna want a progress bar in the console for big datasets, I'll get that in an update
+
     # atom text editor
 wget https://atom-installer.github.com/v1.57.0/atom-amd64.deb
 sudo dpkg -i atom-amd64.deb
