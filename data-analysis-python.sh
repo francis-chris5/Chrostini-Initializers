d74ff5cbf36788fe229f3d82239438aa0fae0112
francis-chris5 <50467171+francis-chris5@users.noreply.github.com>
2021-12-30 00:47:07 -0500

    Add files via upload

---
+++ data-analysis-python.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+
+    ### SET UP WORKING DIRECTORIES
+mkdir ~/Documents	## place to organize projects
+mkdir ~/Documents/delete	## for general scrap/scratch/temporary files
+ln -s /mnt/chromeos/MyFiles/Downloads/	## access to ChromeOS downloads folder
+ln -s /mnt/chromeos/GoogleDrive/MyDrive/ ## access to Google Drive
+
+
+    ### Update and Upgrade apt
+sudo apt update
+sudo apt upgrade -y
+
+
+    ### INSTALL LANGUAGES and TOOLS
+sudo apt install python3-pip -y
+sudo pip3 install numpy scipy matplotlib pandas jupyter
+sudo apt install spyder -y
+
+    # atom text editor
+wget https://atom-installer.github.com/v1.57.0/atom-amd64.deb
+sudo dpkg -i atom-amd64.deb
+sudo apt -f install -y
+rm atom-amd64.deb
